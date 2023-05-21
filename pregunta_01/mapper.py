30beeed9b7c85db52ec24e040e3c7579ba5e8fae
marredondor <marredondor@unal.edu.co>
2023-05-21 11:38:48 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,12 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+# Itera sobre las líneas de entrada
+for line in sys.stdin:
+    # Elimina espacios en blanco y divide por comas
+    data = line.strip().split(',')
+    
+    # Extrae el valor del atributo "credit_history" (suponiendo que es el tercer atributo)
+    credit_history = data[2]
+    
+    # Genera la salida en formato clave-valor: (credit_history, 1)
+    print(credit_history + '\t' + '1')
