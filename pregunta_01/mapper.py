524dac861b460416b5abebb0304c1610abdd5c22
marredondor <marredondor@unal.edu.co>
2023-05-24 16:56:10 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,23 +1,6 @@
 import sys
 
-# Leer el contenido de la entrada estándar
-data = sys.stdin.read()
-
-# Eliminar saltos de línea adicionales
-data = data.strip()
-
-# Dividir el contenido en líneas
-lines = data.split('\n')
-
-# Iterar sobre cada línea de entrada
-for line in lines:
-    # Elimina espacios en blanco y divide la línea en campos
-    fields = line.strip().split(',')
-
-    # Verifica si la línea tiene el número correcto de campos
-    if len(fields) == 21:
-        # Obtiene el valor del atributo 'credit_history'
-        credit_history = fields[2]
-
-        # Emite el par clave-valor al stdout
-        sys.stdout.write(credit_history+"\t1\n")
+for l in sys.stdin:
+  lista = l.split(",")
+  nombre = lista[2]
+  sys.stdout.write(nombre+"\t1\n")
