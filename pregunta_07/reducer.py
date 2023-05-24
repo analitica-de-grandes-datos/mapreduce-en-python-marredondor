d38fd3932a331730c94a163f2d2f9dbd862dcc05
marredondor <marredondor@unal.edu.co>
2023-05-23 23:00:10 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,8 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+datos = []
+for line in sys.stdin:
+    columnas = line.strip().split("\t")
+    datos.append(columnas)
+organizar = sorted(datos, key=lambda x: (x[0], float(x[2])))
+for item in organizar:
+    sys.stdout.write(f"{item[0]}\t{item[1]}\t{item[2]}\n")
