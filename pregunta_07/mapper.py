a3a16167c2b279fd82b030c28f0d1627723f1981
marredondor <marredondor@unal.edu.co>
2023-05-23 22:59:57 -0500

    Update mapper.py

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,8 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+for line in sys.stdin:
+    columnas=line.split(" ")
+    columna1= columnas[0]
+    columna2= columnas[3]
+    columna3= int(columnas[6])
+    sys.stdout.write(f"{columna1}\t{columna2}\t{columna3}\n")
