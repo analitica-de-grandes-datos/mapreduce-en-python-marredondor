a5308e402c450bd371743c5b2f54c4dd8785aaaf
Maximiliano <marredondor@unal.edu.co>
2023-05-26 16:31:06 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,10 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+for row in sys.stdin:
+    elementos = row.strip().split(",")
+    campos = elementos[0]
+    column = campos.split("   ")    
+    linea = column[1]
+    caompos_fecha = linea.split("-")  
+    mes = caompos_fecha[1]
+    sys.stdout.write((mes+"\t1\n"))
