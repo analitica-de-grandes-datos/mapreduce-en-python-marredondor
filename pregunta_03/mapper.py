5c3d1f12414b5508ab5959bbc84b6c6e0bdc572b
Maximiliano <marredondor@unal.edu.co>
2023-05-26 15:32:55 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,23 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+# Leer el contenido de la entrada estándar
+data = sys.stdin.read()
+
+# Eliminar saltos de línea adicionales
+data = data.strip()
+
+# Dividir el contenido en líneas
+lines = data.split('\n')
+
+# Iterar sobre cada línea de entrada
+for line in lines:
+    # Eliminar espacios en blanco y dividir la línea en campos
+    fields = line.strip().split(',')
+
+    # Verificar si la línea tiene el número correcto de campos
+    if len(fields) == 2:
+        # Obtener el valor de la segunda columna
+        column2_value = int(fields[1])
+
+        # Emitir el par clave-valor al stdout
+        sys.stdout.write("{}\t{}\n".format(column2_value, line))
