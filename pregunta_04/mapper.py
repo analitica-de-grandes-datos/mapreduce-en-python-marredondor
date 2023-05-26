eaa2c176a9530bd34314afc2c4357b9b01c4e625
Maximiliano <marredondor@unal.edu.co>
2023-05-26 16:06:55 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,9 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+for row in sys.stdin:
+    elementos = row.strip().split(",")
+    campos = elementos[0]
+    column = campos.split("   ")
+    linea = column[0]
+    #linea = ",".join(column)
+    sys.stdout.write((linea+"\t1\n"))
