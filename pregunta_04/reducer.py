6f300585c511c234dccbbbd0c4b8567bb284f468
Maximiliano <marredondor@unal.edu.co>
2023-05-26 16:06:58 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,24 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+# Inicializar el diccionario para almacenar los conteos de credit_history
+credit_counts = {}
+
+# Leer los datos de la entrada estándar
+for line in sys.stdin:
+    # Eliminar espacios en blanco y dividir la línea en clave y valor
+    credit_history, count = line.strip().split('\t')
+
+    # Convertir el conteo a entero
+    count = int(count)
+
+    # Incrementar el contador para el tipo de credit_history actual
+    if credit_history in credit_counts:
+        credit_counts[credit_history] += count
+    else:
+        credit_counts[credit_history] = count
+
+# Imprimir los resultados
+
+for credit_history in sorted(credit_counts.keys()):
+    count = credit_counts[credit_history]
+    sys.stdout.write("{}\t{}\n".format(credit_history, count))
