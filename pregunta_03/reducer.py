54144bd62fb37347bdf14c8c47a3589bc08f32c5
Maximiliano <marredondor@unal.edu.co>
2023-05-26 15:32:57 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,11 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+# Leer los datos de la entrada estándar
+lines = sys.stdin.readlines()
+
+# Ordenar las líneas por la clave (segunda columna)
+sorted_lines = sorted(lines, key=lambda x: int(x.strip().split('\t')[0]))
+
+# Imprimir las líneas ordenadas
+for line in sorted_lines:
+    sys.stdout.write(line.split('\t')[1])
