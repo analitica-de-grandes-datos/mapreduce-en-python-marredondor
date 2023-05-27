f646b03b5c66ec21cf350dbec9d73948aa1a77eb
Maximiliano <marredondor@unal.edu.co>
2023-05-26 22:01:51 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,25 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+# Inicializar el diccionario para almacenar los valores máximo y mínimo de la tercera columna por letra
+max_values = {}
+min_values = {}
+
+# Leer los datos de la entrada estándar
+for line in sys.stdin:
+    # Eliminar espacios en blanco y dividir la línea en clave y valor
+    clave, valor = line.strip().split('\t')
+    valor = float(valor)
+
+    # Actualizar el valor máximo y mínimo para cada letra
+    if clave in max_values:
+        max_values[clave] = max(max_values[clave], valor)
+        min_values[clave] = min(min_values[clave], valor)
+    else:
+        max_values[clave] = valor
+        min_values[clave] = valor
+
+# Imprimir los resultados
+for clave in sorted(max_values.keys()):
+    max_valor = max_values[clave]
+    min_valor = min_values[clave]
+    sys.stdout.write("{}\t{}\t{}\n".format(clave, max_valor, min_valor))
