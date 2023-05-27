90be63a6bcc97bca5eacf843a4f368c673d35b5e
Maximiliano <marredondor@unal.edu.co>
2023-05-26 22:31:57 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,9 +1,17 @@
 import sys
 
+# Leer los datos de la entrada estándar y almacenarlos en una lista
 datos = []
 for line in sys.stdin:
     columnas = line.strip().split("\t")
     datos.append(columnas)
-organizar = sorted(datos, key=lambda x: (x[0], float(x[2])))
+
+# Ordenar la lista por letra (columna 1) y valor (columna 3)
+organizar = sorted(datos, key=lambda x: (x[0], int((x[2]))))
+
+# Imprimir los resultados
 for item in organizar:
-    sys.stdout.write(f"{item[0]}\t{item[1]}\t{item[2]}\n")
+    sys.stdout.write("{}\t{}\t{}\n".format(item[0], item[1], item[2]))
+    
+# Agregar las líneas adicionales
+sys.stdout.write("")
