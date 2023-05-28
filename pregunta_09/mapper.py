0a80530c16066467cdc1a9c1a42b76cc34f46ea3
Maximiliano <marredondor@unal.edu.co>
2023-05-28 10:25:23 -0500

    Update mapper.py

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,12 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+# Inicializar una lista para almacenar los valores de la tercera columna
+values = []
+
+# Iterar sobre cada línea de entrada
+for row in sys.stdin:
+    # Eliminar espacios en blanco y dividir la línea en campos
+    campos = row.strip().split()
+
+    # Verificar si la línea tiene el número correcto de campos
+    sys.stdout.write("{}\t{}\t{}".format(row.split("   ")[0],row.split("   ")[1],row.split("   ")[2]))
