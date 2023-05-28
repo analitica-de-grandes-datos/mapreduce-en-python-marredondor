45077b916f781d41d326c4cd16ca6e8756c7f85d
Maximiliano <marredondor@unal.edu.co>
2023-05-28 10:16:27 -0500

    Update mapper.py

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,14 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+# Iterar sobre cada línea de entrada
+for row in sys.stdin:
+    # Eliminar espacios en blanco y dividir la línea en campos
+    campos = row.strip().split()
+
+    # Verificar si la línea tiene el número correcto de campos
+    if len(campos) == 3:
+        clave = campos[0]
+        valor = float(campos[2])
+
+        # Emitir el par clave-valor al stdout
+        sys.stdout.write("{}\t{}\n".format(clave, valor))
