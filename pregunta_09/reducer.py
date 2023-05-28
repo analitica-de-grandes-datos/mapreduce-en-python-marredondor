386771719a0b5f2cca95deaa4480105289217aac
Maximiliano <marredondor@unal.edu.co>
2023-05-28 10:25:25 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,16 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+list_of_lists = []
+for line in sys.stdin:
+    line = line.strip()
+    letra, fecha, numero = line.split("\t")
+    new_list = [elem for elem in line.split("\t")]
+    list_of_lists.append(new_list)
+
+for n in range(len(list_of_lists)):
+    list_of_lists[n][2]=int(list_of_lists[n][2])
+
+Listaordenada=sorted(list_of_lists,key = lambda x: (x[2]))
+
+for data in Listaordenada[0:6]:
+    sys.stdout.write("{}   {}   {}\t\n".format(data[0], data[1],data[2]))
