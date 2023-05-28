bb1923fca1fc432d1236f4e8188a9e991ee37fcc
Maximiliano <marredondor@unal.edu.co>
2023-05-28 10:55:11 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,24 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+# Iterar sobre cada línea de entrada recibida a través de sys.stdin
+for line in sys.stdin:
+    # Inicializar una lista vacía para almacenar las sublistas
+    list_of_lists = []
+
+    # Eliminar los caracteres de nueva línea y retorno de carro de la línea
+    line = line.replace('\n', '')
+    line = line.replace('\r', '')
+
+    # Dividir la línea en el índice y las letras utilizando '\t' como separador
+    index, letras = line.split('\t')
+
+    # Dividir las letras en una lista utilizando ',' como separador
+    letras = letras.split(',')
+
+    # Crear sublistas con el índice y cada letra y agregarlas a la lista de listas
+    for letra in letras:
+        list_of_lists.append([index, letra])
+
+    # Emitir los pares clave-valor al stdout en el formato requerido
+    for pareja in list_of_lists:
+        sys.stdout.write("{}\t{}\n".format(str(pareja[1]), str(pareja[0])))
